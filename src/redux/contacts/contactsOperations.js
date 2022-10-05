4fc362b6e30b09d0a92d4e1560132a5628cbccf7
seacat271 <seacat271083@gmail.com>
2022-10-05 20:38:49 +0300

    add rejectWithValue

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -4,35 +4,30 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = 'https://633c5013f11701a65f73ed7d.mockapi.io/contacts';
 
-// export const addContact = contact => async dispatch => {
-//   dispatch(addContactRequest());
-// try {
-//     const response = await axios.post('/contacts', contact);
-//     dispatch(addContactSuccess(response.data))
-    
-// } catch (error) {dispatch(addContactError(error))}
-// };
 
-export const addContact = createAsyncThunk("contact/add", async (contact) => {
-    const response = await axios.post('/contacts', contact);
+export const addContact = createAsyncThunk("contact/add", async (contact, {rejectWithValue}) => {
+    try {
+    const response = await axios.post('/contact', contact);
     return response.data
+    } catch (error) {
+        return rejectWithValue(error)
+    }
 })
 
-// export const deleteContact = id => async dispatch => {
-//     dispatch(deleteContactRequest());
-// try {
-//     await axios.delete(`/contacts/${id}`);
-//     dispatch(deleteContactSuccess(id));
-
-// } catch (error) {dispatch(deleteContactError(error))}
-// };
-export const deleteContact = createAsyncThunk("contact/delete", async (id) => {
+export const deleteContact = createAsyncThunk("contact/delete", async (id, {rejectWithValue}) => {
+try {
     await axios.delete(`/contacts/${id}`);
     return id
+} catch (error) { 
+    rejectWithValue(error)
+}
 })
 
-
-export const fetchContacts = createAsyncThunk("contacts/get", async () => {
+export const fetchContacts = createAsyncThunk("contacts/get", async (_, {rejectWithValue}) => {
+try {
     const response = await axios.get('/contacts');
     return response.data
+} catch (error) {
+    rejectWithValue(error)
+}
 })
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { itemsSlice } from './contacts/slice';
-// import { filterSlice } from './filter/slice';
 import { filterReducer} from './filter/filterReducer';
 import { contactsReducer } from './contacts/contactsReducer';
 
