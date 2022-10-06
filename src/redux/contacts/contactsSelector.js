f7b80d2ae1d477e224bd57547f886392b59fc7ec
seacat271 <seacat271083@gmail.com>
2022-10-06 20:18:11 +0300

    add memoSelector

--- src/redux/contacts/contactsSelector.js
+++ src/redux/contacts/contactsSelector.js
@@ -1,13 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
 export const getItems = state => state.contacts.items;
 export const getFilter = state => state.filter;
 
-export const getVisibleContacts = (state) => {
-    const items = getItems(state);
-    const filter = getFilter (state);
-    const filterNormalize = filter.toLowerCase(); 
-    return (filter) 
+
+  export const getVisibleContacts = createSelector(
+    [getItems, getFilter],
+    (items, filter) => {
+      const filterNormalize = filter.toLowerCase();
+      return (filter) 
     ? items.filter(contact => contact.name.toLowerCase().includes(filterNormalize))
     : items
-  };
-
-  
+    }
+  );
