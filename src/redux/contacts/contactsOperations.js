9fd999f93ede14494f017e6deeaebe494e168140
seacat271 <seacat271083@gmail.com>
2022-10-06 14:38:22 +0300

    add apiFetch

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,22 +1,18 @@
-import axios from 'axios';
-
 import {createAsyncThunk} from "@reduxjs/toolkit";
-
-axios.defaults.baseURL = 'https://633c5013f11701a65f73ed7d.mockapi.io/contacts';
-
+import { addContactFetch, deleteContactFetch, allContactFetch } from "utils/mockApi";
 
 export const addContact = createAsyncThunk("contact/add", async (contact, {rejectWithValue}) => {
     try {
-    const response = await axios.post('/contacts', contact);
+    const response = await addContactFetch(contact);
     return response.data
     } catch (error) {
         return rejectWithValue(error)
     }
-})
+}, {})
 
 export const deleteContact = createAsyncThunk("contact/delete", async (id, {rejectWithValue}) => {
 try {
-    await axios.delete(`/contacts/${id}`);
+    await deleteContactFetch(id);
     return id
 } catch (error) { 
     rejectWithValue(error)
@@ -25,7 +21,7 @@ try {
 
 export const fetchContacts = createAsyncThunk("contacts/get", async (_, {rejectWithValue}) => {
 try {
-    const response = await axios.get('/contacts');
+    const response = await allContactFetch();
     return response.data
 } catch (error) {
     rejectWithValue(error)
